ef1b35b5a2339053b1c6b14ea59ae4a948b4fab5
pleasant8547 <pleasant8547@gmail.com>
2017-05-08 16:18:23 +0900

    Lotto

--- study/src/com/alone/lotto/Lotto.java
+++ study/src/com/alone/lotto/Lotto.java
@@ -2,7 +2,19 @@ package com.alone.lotto;
 
 public class Lotto {
 
+	//1. Collections.shuffle() 이용한 로또 번호 추출하기
+	
+	
+	/*
+▼ 프로그램의 진행은 로또 번호를 몇 개 뽑아낼 것인지 묻습니다. 그러면 입력한 숫자 만큼 번호를 추출하게 됩니다. 
+for 문을 이용해서 List 객체에 번호들을 모두 추가합니다. 
+추가후 Collections.shuffle() 함수의 인수로 넘기게 되면 자동으로 섞어줍니다. 
+그럼 리스트 안에는 섞은 값이 차례대로 추가 되어 있을 겁니다. 이것을 하나씩 꺼내시면 되겠죠. 
+이렇게 꺼낸 값을 숫자의 숫서대로 정렬하기 위해 Array.sort() 함수를 이용했습니다.
+
+*/	
 	public static void main(String[] args) {
+		
 		
 
 	}
