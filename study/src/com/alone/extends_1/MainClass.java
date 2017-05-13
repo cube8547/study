27abe4bc18589c753272d775d24cbf8172658726
pleasant8547 <pleasant8547@gmail.com>
2017-05-13 14:04:27 +0900

    상속

---
+++ study/src/com/alone/extends_1/MainClass.java
@@ -0,0 +1,17 @@
+package com.alone.extends_1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		ChildMenu childMenu = new ChildMenu();
+		childMenu.makeChungGukJang();
+		childMenu.makeDoenJangGuk();
+		childMenu.makeGalbiJjim();
+		childMenu.makeHotDoenJangGuk();
+		childMenu.makeKongNaMool();
+		childMenu.MakeSoybean();
+
+	}
+
+}
