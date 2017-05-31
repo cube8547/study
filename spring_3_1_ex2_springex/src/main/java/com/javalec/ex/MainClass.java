091791661e9b83aa487d34b9d720b4398766a62d
pleasant8547 <pleasant8547@gmail.com>
2017-06-01 08:07:49 +0900

    스프링 적용과 안된 예 스프링을 알기 쉽게 예전 방식으로 풀이

---
+++ spring_3_1_ex2_springex/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,34 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		/* 스프링 적용 안된 예
+		MyCalculator myCalculator = new MyCalculator();
+		myCalculator.setCalculator(new Calculator());
+		
+		myCalculator.setFirstNum(10);
+		myCalculator.setSecondNum(2);
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+		*/
+		//스프링으로 만든 예
+		String configLocation = "classpath:applicationCTX.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		MyCalculator myCalculator = ctx.getBean("myCalculator", MyCalculator.class);//getBean을 얻어와서 myCalculator에 담는다
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+		
+	}
+	
+}
